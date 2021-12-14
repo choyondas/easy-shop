72141a2cd74fb6e6fb183bbaf275681a36d39e04
choyondas <choyondas08@gmail.com>
2021-12-14 20:35:42 +0800

    second

--- src/Pages/HomePage/Homepage.js
+++ src/Pages/HomePage/Homepage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Announcement from "../../components/Announcement/Announcement";
 import Navbar from "../Shared/Navbar/Navbar";
 
 const Homepage = () => {
   return (
     <div className="container">
+      <Announcement></Announcement>
       <Navbar></Navbar>
     </div>
   );
