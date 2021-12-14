4c77b95168dcfedaa8a26fc49b8bf5781dfc004d
choyondas <choyondas08@gmail.com>
2021-12-14 19:21:28 +0800

    first

---
+++ src/Pages/HomePage/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import "./Navbar.css";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Navbar = () => {
+  return <Container>navbar</Container>;
+};
+
+export default Navbar;
